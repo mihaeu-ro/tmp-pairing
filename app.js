8293e229affc94dc80fba7f32d2a1e9b25ea57cc
Michael Haeuslmann <haeuslmann@gmail.com>
2015-10-13 11:16:47 +0200

    Added bower and more node components.

--- app.js
+++ app.js
@@ -50,7 +50,10 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.send('ERROR!!!!');
+    res.render('error', {
+    message: err.message,
+    error: {}
+  });
   });
 }
 
